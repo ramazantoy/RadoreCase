06cea6029f055c34ec76a26a4216d827c2ae1031
Ramazan  TOY <ramcotoy06@gmail.com>
2023-06-16 14:12:35 +0300

    get all accounts trys

--- RadoreApiController/RadoreApiController.cs
+++ RadoreApiController/RadoreApiController.cs
@@ -1,13 +1,46 @@
-﻿namespace RadoreCaseRamazan.RadoreApiController;
+﻿using Newtonsoft.Json;
+using RestSharp;
+
+namespace RadoreCaseRamazan.RadoreApiController;
 
 public class RadoreApiController
 {
     private static readonly Lazy<RadoreApiController> _instance = new Lazy<RadoreApiController>(() => new RadoreApiController());
+
+    public static RadoreApiController Instance => _instance.Value;
     
+    private RestClient _client;
+
+ 
+
     private RadoreApiController()
     {
+        _client = new RestClient("https://intestapi.radore.com/api");
+    
         // Bağlantı ayarları veya diğer ilgili kodlar buraya gelebilir
     }
 
-    public static RadoreApiController Instance => _instance.Value;
+    public string? GetAllAccounts(string endpoint)
+    {
+        var request = new RestRequest(endpoint);
+        
+        string? allMembers = _client.Execute(request).Content;
+        
+        Console.WriteLine(allMembers);
+        
+        return allMembers;
+    }
+
+    public List<string>? GetAllAccountsToList(string endpoint)
+    {
+        var request = new RestRequest(endpoint);
+        
+        string? allMembers = _client.Execute(request).Content;
+
+
+        return allMembers != null ? JsonConvert.DeserializeObject<List<string>>(allMembers) : null;
+    }
+
+
+
 }
