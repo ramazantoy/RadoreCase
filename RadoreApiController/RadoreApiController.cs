b0f7f6940b3fb4b1fe708b535fe13d421d4b381a
Ramazan  TOY <ramcotoy06@gmail.com>
2023-06-16 13:06:00 +0300

    radore api controller class created

---
+++ RadoreApiController/RadoreApiController.cs
@@ -0,0 +1,13 @@
+﻿namespace RadoreCaseRamazan.RadoreApiController;
+
+public class RadoreApiController
+{
+    private static readonly Lazy<RadoreApiController> _instance = new Lazy<RadoreApiController>(() => new RadoreApiController());
+    
+    private RadoreApiController()
+    {
+        // Bağlantı ayarları veya diğer ilgili kodlar buraya gelebilir
+    }
+
+    public static RadoreApiController Instance => _instance.Value;
+}
